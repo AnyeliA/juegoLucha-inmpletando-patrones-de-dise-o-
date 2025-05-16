6e043ff52c36910a996c7bde926d9be44c94ce49
Isabela Gonzalez <159276237+BelaIsBelaG@users.noreply.github.com>
2025-05-16 12:53:02 +0000

    Actualizacion de la clase BolaFuego.java

--- BolaFuego.java
+++ BolaFuego.java
@@ -1,6 +1,8 @@
+import java.util.Random;
+
 public class BolaFuego implements Ataque {
-  
+    @Override
     public int ejecutar() {
         return new Random().nextInt(16) + 15; // 15 a 30
     }
-}    
+}
