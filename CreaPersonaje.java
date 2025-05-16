1fed33f220ca6858b0f9debfbf8c2f4762ace336
IYCortes <166848478+IYCortes@users.noreply.github.com>
2025-05-16 06:32:02 +0000

    Eliminando clase CreaPersonaje.java

--- CreaPersonaje.java
+++
@@ -1,7 +0,0 @@
-//creaPersonaje.java
-public class crearPersonaje extends FabricaPersonaje {
-    @Override
-    public Personaje crearPersonaje(String nombre) {
-        return new Guerrero(nombre);
-    }
-}
